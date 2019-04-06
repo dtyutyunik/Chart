a6fb5e5def77c4a32ff81b741e675d4e7142440b
Dmitriy Tyutyunik <dmitriy.tyutyunik@gmail.com>
2019-04-05 20:46:15 -0400

    hosted on financial_calc.surge.sh

--- client/src/components/PieChart.js
+++ client/src/components/PieChart.js
@@ -2,27 +2,6 @@ import React, { Component } from 'react';
 import {Bar, Line, Pie} from 'react-chartjs-2';
 
 
-const data = {
-	labels: [
-		'Red',
-		'Green',
-		'Yellow'
-	],
-	datasets: [{
-		data: [300, 50, 100],
-		backgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-		'#FFCE56'
-		],
-		hoverBackgroundColor: [
-		'#FF6384',
-		'#36A2EB',
-		'#FFCE56'
-		]
-	}]
-};
-
 export default function PieChart(props){
   let expense=0;
    props.expenses.forEach(function(i){
